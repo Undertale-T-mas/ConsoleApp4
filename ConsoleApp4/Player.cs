4933a4abf9aae951debcee1928371aaf0f5374a4
T-mas <78857643+Undertale-T-mas@users.noreply.github.com>
2024-03-01 08:42:04 +0800

    添加项目文件。

---
+++ ConsoleApp4/Player.cs
@@ -0,0 +1,73 @@
+﻿namespace Test
+{
+    public struct Vec2
+    {
+        public float x, y;
+
+        public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+    }
+
+    public class Entity
+    {
+        public Vec2 position;
+        public float radius;
+
+        public virtual bool IsCollide(Entity entity)
+        {
+            float deltaX = entity.position.x - position.x;
+            float deltaY = entity.position.y - position.y;
+
+            float distance = MathF.Sqrt(deltaY * deltaY + deltaX * deltaX);
+            return distance < radius + entity.radius;
+        }
+    }
+    public interface Hitable
+    {
+        void Hit(int damage);
+    }
+    public class Bullet : Entity
+    {
+        public int damage;
+
+        private void HitEntity(Hitable hitable)
+        {
+            hitable.Hit(damage);
+        }
+        public void CheckHit(Entity other)
+        {
+            if (other is not Hitable a) return;
+
+            if(other.IsCollide(this))
+            {
+                HitEntity(a);
+            }
+        }
+    }
+    public class Player : Entity, Hitable
+    {
+        public int _id;
+        public string _name;
+
+        public int hp = 10;
+
+        public void Hit(int damage)
+        {
+            hp -= damage;
+            if (hp <= 0) Console.WriteLine("Player is dead!");
+        }
+
+        public override string ToString()
+        {
+            return $"The player has name {_name}, id = {_id}";
+        }
+
+        public override bool IsCollide(Entity entity)
+        {
+            if(this.hp <= 0) return false;
+            return base.IsCollide(entity);
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using Test;
+
+Player p = new();
+p._name = "test";
+p._id = 1;
+Player q = p;
+q._id += 1;
+Console.WriteLine(q);
+q.position.x = 0;
+q.position.y = 0;
+q.radius = 5;
+Bullet bullet = new();
+bullet.position.x = 2;
+bullet.position.y = 2;
+bullet.radius = 5;
+
+Console.WriteLine(bullet.position);
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"damage = {i}");
+    bullet.damage = i;
+
+    bullet.CheckHit(q);
+}
